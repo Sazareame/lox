cc5a91b0272c20ab34d503345a6e488f659260e9
aisis <busysuzume@gmail.com>
2024-10-12 20:35:13 +0800

    rlox: constant opcode

--- rlox/src/chunk.rs
+++ rlox/src/chunk.rs
@@ -1,41 +1,124 @@
-macro_rules! def_and_display_enum {
-  ($name:ident, $($variant:ident),*) => {
-    #[derive(Clone)]
+#[macro_use]
+mod macro_def{
+macro_rules! clean {
+    ($typ:ty) => { _ };
+}
+macro_rules! def_opcode {
+  ($name:ident, $($variant:ident$(($($carry:ty),+))?),+) => {
+    #[repr(C)]
     pub enum $name {
-      $($variant),*
+      $($variant$(($($carry),+))?),*
+    }
+
+    impl $name {
+      pub fn to_int(&self) -> u32 {
+        unsafe{*<*const _>::from(self).cast()}
+      }
     }
 
     impl std::fmt::Display for $name {
       fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use crate::chunk::$name::*;
         match self {
-          $($variant => write!(f, "{}", stringify!($variant).to_ascii_uppercase())),*
+          $($variant$(($(clean!($carry)),+))? => write!(f, "{:04} OP_{}", self.to_int(), stringify!($variant).to_ascii_uppercase())),+
         }
       }
     }
   }
 }
+}
 
-def_and_display_enum!(OpCode, Return);
 
+def_opcode!(OpCode, Return, Constant(u8));
+use crate::value::Value;
+
+// #[repr(C)]
+// pub enum OpCode {
+//   Return,
+//   Constant(u8),
+// }
+// 
+// impl OpCode {
+//   pub fn to_int(&self) -> u32 {
+//     unsafe{*<*const _>::from(self).cast()}
+//   }
+// }
+// 
+// impl std::fmt::Display for OpCode {
+//   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//     use OpCode::*;
+//     match self{
+//       Return => write!(f, "{:04} OP_RETURN", self.to_int()),
+//       Constant(i) => wri
+//     }
+//   }
+// }
+
+/// Constant Pool used to store constant define by OP_CONSTANT,  
+/// The OP_CONSTANT could access the value it refers to by the u8 it carried as index.
+struct ConstantPool{
+  constants: Vec<Value>,
+}
+
+impl ConstantPool{
+  pub fn new() -> Self{
+    Self{constants: Vec::new()}
+  }
+
+  /// Add a constant to the pool, then return its index in the underlaying data buffer.  
+  /// This mothod is now use u8 as return value, which means that the index's max value is 255.
+  pub fn add_constant(&mut self, val: Value) -> u8{
+    self.constants.push(val);
+    (self.constants.len() - 1).try_into().unwrap()
+  }
+
+  /// Retrievl the constant value via index.  
+  /// This method assert that the given index is always valid and do no bound-checking.  
+  /// Also, the index is represented by u8.
+  pub fn get_constant(&self, index: u8) -> Value{
+    unsafe{*self.constants.get_unchecked(index as usize)}
+  }
+}
+
+/// Chunk used to store OpCodes and a constant pool.
 pub struct Chunk {
-  chunk: Vec<OpCode>,
+  chunks: Vec<OpCode>,
+  constants: ConstantPool,
 }
 
 impl Chunk {
   pub fn new() -> Self {
-    Self { chunk: Vec::new() }
+    Self { 
+      chunks: Vec::new(),
+      constants: ConstantPool::new(),
+    }
   }
 
+  /// Add an OpCode into the underlying data buffer hold by Chunk.
   pub fn write_chunk(&mut self, code: OpCode) {
-    self.chunk.push(code);
+    self.chunks.push(code);
   }
 
-  pub fn disassembley(&self, title: &str) {
+  /// Add a constant value into the constant pool it contains, then return the index of that constant in the pool.  
+  /// This return value is what is carried by the OpCode Enum.
+  pub fn write_constant(&mut self, val: Value) -> u8 {
+    self.constants.add_constant(val)
+  }
+
+  /// Display the opcodes in Chunk by lines, with additional information if exists.
+  pub fn disassembly(&self, title: &str) {
     println!("== {} ==", title);
     self
-      .chunk
+      .chunks
       .iter()
-      .for_each(|code| println!("{:04X} {}", (code.clone() as usize), code));
+      .for_each(|code| self.disassembly_ins(code));
+  }
+
+  fn disassembly_ins(&self, ins: &OpCode) {
+    use OpCode::*;
+    match ins{
+      Constant(i) => println!("{}'{}", ins, self.constants.get_constant(*i)),
+      _ => println!("{}", ins),
+    }
   }
 }
--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,7 +1,11 @@
 mod chunk;
+mod value;
 use chunk::*;
 fn main() {
   let mut chunk = Chunk::new();
   chunk.write_chunk(OpCode::Return);
-  chunk.disassembley("test");
+  let offset = chunk.write_constant(3.14);
+  chunk.write_chunk(OpCode::Constant(offset));
+  chunk.write_chunk(OpCode::Return);
+  chunk.disassembly("test");
 }
